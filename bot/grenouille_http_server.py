cf20e57e3676b1bae4a2f17e32ced018bab1cc13
Yannis Berrouag <yannis.berrouag@gmail.com>
2016-11-18 16:15:44 +0000

    add secret key security to use app

--- bot/grenouille_http_server.py
+++ bot/grenouille_http_server.py
@@ -5,24 +5,45 @@ import cgi
 import os
 
 
-def MakeHandler(bot):
+def MakeHandler(grenouille_bot):
     class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         
         def do_POST(self):
-            form = cgi.FieldStorage(
-                fp=self.rfile,
-                headers=self.headers,
-                environ={"REQUEST_METHOD": "POST"}
-            )
-            
-            for item in form.list:
-                if (item.name == "say"):
-                    bot.send_msg(item.value)
+            try:
+                form = cgi.FieldStorage(
+                    fp=self.rfile,
+                    headers=self.headers,
+                    environ={"REQUEST_METHOD": "POST"}
+                )
+
+                headerkey = 'X-Grenouille-Api-Key'
+                if (not headerkey in self.headers):
+                    return self.unauthorized()
+                
+                key, values = cgi.parse_header(self.headers[headerkey])
+                
+                if (key != grenouille_bot.grenouille_bot.config['DEFAULT']['grenouille_api_key']):
+                    return self.unauthorized()
                     
-                    self.send_response(200)
-                    self.send_header("Content-type", "text/html")
-                    self.end_headers()
-                    return
+                    
+                for item in form.list:
+                    if (item.name == "say"):
+                        grenouille_bot.bot.send_msg(item.value)
+                        
+                        self.send_response(200)
+                        self.send_header("Content-type", "text/html")
+                        self.end_headers()
+                        return
+            except Exception as e:
+                logging.info(e)
+
+
+        def unauthorized(self):
+            self.send_response(403)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            return
+        
     return testHTTPServer_RequestHandler
 
 class GrenouilleHttpServer:
@@ -42,7 +63,7 @@ class GrenouilleHttpServer:
         try :
             port = int(os.environ['WEBSERVER_PORT'])
             server_address = ('127.0.0.1', port)
-            myhandler = MakeHandler(self.bot)
+            myhandler = MakeHandler(self)
             httpd = HTTPServer(server_address, myhandler)
             logging.info('webserver started on port ' + str(port))
             httpd.serve_forever()
